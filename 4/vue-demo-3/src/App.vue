ea5b8d347e9597e5ddac74cfc31c7a226ea1e080
cjjWindows <beHumbleplz@yeah.net>
2024-01-29 22:37:27 +0800

    1月29内容

---
+++ 4/vue-demo-3/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3>v-model原理</h3>
+  <!-- 
+    v-model实际上是一个语法糖:
+      :value 搭配 input 事件的双向绑定
+   -->
+  <input type="text" v-model="msg1" />
+  <br>
+  <input type="text" :value="msg1" @input="$event.target.value = msg1" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg1: "msg1",
+      msg2: "msg2",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
